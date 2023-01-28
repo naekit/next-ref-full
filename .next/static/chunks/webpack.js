706ee0bfc5c71c69b4a05405cef7dbcb1e4dd45f
naekit <mykyta.onishchuck@code.berlin>
2023-01-27 21:22:34 -0500

    added new event page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9fbd20f2ef62ce3a"; }
+/******/ 		__webpack_require__.h = function() { return "6d09fed95dbe4043"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,32 @@
 import React from "react"
+import MeetupList from "../components/meetups/MeetupList"
 
-const index = () => {
-	return <div>index</div>
+const SampleEvents = [
+	{
+		id: 1,
+		title: "First Event",
+		image: "https://images.unsplash.com/photo-1530743373890-f3c506b0b5b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1535&q=80",
+		address: "Some Address",
+		description: "This is a first event",
+	},
+	{
+		id: 2,
+		title: "Second Event",
+		image: "https://images.unsplash.com/photo-1530743373890-f3c506b0b5b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1535&q=80",
+		address: "Some Address",
+		description: "This is a second event",
+	},
+	{
+		id: 3,
+		title: "Third Event",
+		image: "https://images.unsplash.com/photo-1530743373890-f3c506b0b5b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1535&q=80",
+		address: "Some Address",
+		description: "This is a third event",
+	},
+]
+
+const HomePage = () => {
+	return <MeetupList meetups={SampleEvents} />
 }
 
-export default index
+export default HomePage
